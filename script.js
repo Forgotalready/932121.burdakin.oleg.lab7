3ecb2ae107c388b533fe423b0e1963bbf307d716
Oleg <oleg.burdakin2016@yandex.ru>
2024-10-20 13:40:17 +0700

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+function drawRegularPolygon(vertexNum)
+{
+    const canvas = document.querySelector(".canvas")
+    const context = canvas.getContext("2d")
+    context.fillStyle = "green"
+    context.fillRect(10, 10, 50, 50)
+}
